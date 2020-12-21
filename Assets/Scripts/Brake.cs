0dd03d1659eae32b643e13d47ca023e3fd58717f
KaroDev <karodev3@gmail.com>
2020-12-20 20:41:33 -0500

    Task 2: CameraController.cs

--- Assets/Scripts/Brake.cs
+++ Assets/Scripts/Brake.cs
@@ -4,13 +4,15 @@ public class Brake : MonoBehaviour
 {
     public Rigidbody rb;
     public SpriteRenderer spriteRenderer;
+    Vector3 pos = new Vector3(0, 0.28f, 0);
 
     void Update()
     {
-        Debug.Log(rb.velocity.magnitude);
         if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0 && rb.velocity.magnitude > 0.1f)
         {
-            transform.position = new Vector3(rb.transform.position.x, 0.28f, rb.transform.position.z);
+            pos.x = rb.transform.position.x;
+            pos.z = rb.transform.position.z;
+            transform.position = pos;
             spriteRenderer.enabled = true;
         }
         else
