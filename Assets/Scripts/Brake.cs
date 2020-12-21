7eacb2c7931893f6a50c0330ddd99a0ec15ffc5a
KaroDev <karodev3@gmail.com>
2020-12-20 20:25:24 -0500

    Task 1: PlayerController.cs

---
+++ Assets/Scripts/Brake.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Brake : MonoBehaviour
+{
+    public Rigidbody rb;
+    public SpriteRenderer spriteRenderer;
+
+    void Update()
+    {
+        Debug.Log(rb.velocity.magnitude);
+        if(Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0 && rb.velocity.magnitude > 0.1f)
+        {
+            transform.position = new Vector3(rb.transform.position.x, 0.28f, rb.transform.position.z);
+            spriteRenderer.enabled = true;
+        }
+        else
+            spriteRenderer.enabled = false;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+    Vector3 forceDirection;
+    Vector3 rotationDirection;
+
+	// Start is called before the first frame update
+	void Start() => rb = GetComponent<Rigidbody>();
+
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+            // movement
+            forceDirection.x = Input.GetAxis("Horizontal");
+            forceDirection.z = Input.GetAxis("Vertical");
+            rb.AddForce(forceDirection * speed * Time.deltaTime);
+
+            // rotation
+            rotationDirection.x = Input.GetAxis("Vertical");
+            rotationDirection.z = -Input.GetAxis("Horizontal");
+            transform.Rotate(rotationDirection * (speed * 3) * Time.deltaTime);
+    }
+}
